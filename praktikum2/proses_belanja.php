7641f784e0facca984013cf87c8f607a9c28cb1d
FurqonRizqi01 <furqonrizqi0@gmail.com>
2023-03-13 14:08:28 +0700

    praktikum 2

--- praktikum2/proses_belanja.php
+++ praktikum2/proses_belanja.php
@@ -1,17 +1,22 @@
 <?php 
 
 $customer = $_POST['customer'];
-$produk = $_POST['produk'];
 $jumlah = $_POST['jumlah'];
 
-if ($produk = "TV"){
+if($_POST["produk"] == "TV" ){
     $harga = 4200000;
-}
-if ($produk = "Kulkas"){
+} elseif($_POST["produk"] == "Kulkas") {
     $harga = 3100000;
-}
-if ($produk = "Mesin Cuci"){
+} elseif($_POST["produk"] == "Mesin Cuci") {
     $harga = 3800000;
+}
+
+if($_POST["produk"] == "TV"  ){
+    $produk = "TV";
+} elseif($_POST["produk"] == "Kulkas") {
+    $produk = "Kulkas";
+} elseif($_POST["produk"] == "Mesin Cuci" ) {
+    $produk = "Mesin Cuci";
 }
 
 $total_belanja = $harga * $jumlah
